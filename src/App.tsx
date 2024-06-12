a6209d6dcd2e2008c005beb0cff1d1178023e193
Stephan109-5 <t109990005@ntut.org.tw>
2024-06-12 23:04:09 +0800

    fix some stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-// import { useEffect, useState } from "react";
-import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-// import type { Schema } from "../amplify/data/resource";
-// import { generateClient } from "aws-amplify/data";
 import { Route, Routes } from "react-router-dom";
 import Todo from "./todo";
 import Storage_Page from './storage_page';
@@ -10,19 +6,13 @@ import Storage_Page from './storage_page';
 // const client = generateClient<Schema>();
 
 function App() {
-
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
         <Routes>
           <Route path="/">
-            <Route index element={<Todo signOut={signOut} user={user} />} />
+            <Route index element={<Todo/>} />
             <Route path='storing' element={<Storage_Page/>} />
           </Route>
         </Routes>
-        
-      )}
-    </Authenticator>
   );
 }
 

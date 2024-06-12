fe27117200c57e1b4208105347bbc118ff09813e
Stephan109-5 <t109990005@ntut.org.tw>
2024-06-12 22:42:55 +0800

    added user isolation

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,26 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
+import { Route, Routes } from "react-router-dom";
+import Todo from "./todo";
+import Storage_Page from './storage_page';
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
-  }
 
   return (
     <Authenticator>
-      {({ signOut }) => (
-        <main>
-          <h1>My todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id} onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
-            ))}
-          </ul>
-          <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review next step of this tutorial.
-            </a>
-          </div>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-
+      {({ signOut, user }) => (
+        <Routes>
+          <Route path="/">
+            <Route index element={<Todo signOut={signOut} user={user} />} />
+            <Route path='storing' element={<Storage_Page/>} />
+          </Route>
+        </Routes>
+        
       )}
     </Authenticator>
   );
---
+++ src/storage_page.tsx
@@ -0,0 +1,10 @@
+
+export default function Storage_Page() {
+
+
+    return (
+        <main>
+            <h1>Hello</h1>
+        </main>
+    )
+}

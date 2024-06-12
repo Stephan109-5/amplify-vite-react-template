7a1bf991f7b7cbb700960018dda157586e4042bb
Stephan109-5 <t109990005@ntut.org.tw>
2024-06-13 00:37:45 +0800

    added storage upload

--- src/storage_page.tsx
+++ src/storage_page.tsx
@@ -1,10 +1,43 @@
+import React from 'react';
+import { uploadData } from 'aws-amplify/storage';
+import { Authenticator } from '@aws-amplify/ui-react'
+import { StorageManager } from '@aws-amplify/ui-react-storage';
 
 export default function Storage_Page() {
+    const [file, setFile] = React.useState();
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const handleChange = (event: any) => {
+        setFile(event.target.files[0]);
+    };
 
     return (
-        <main>
-            <h1>Hello</h1>
-        </main>
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>{user?.signInDetails?.loginId}'s Storage</h1>
+                    <div>
+                        <input type="file" onChange={handleChange} />
+                        <button
+                            onClick={() =>
+                                uploadData({
+                                    path: `picture-submissions/${file.name}`,
+                                    data: file,
+                                })
+                            }
+                        >
+                            Upload
+                        </button>
+                        <StorageManager
+                            acceptedFileTypes={['image/*']}
+                            path="picture-submission/"
+                            maxFileCount={1}
+                            isResumable
+                        />
+                    </div>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
     )
 }

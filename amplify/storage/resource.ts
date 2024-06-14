6dfe95a8405be74358043ebc4301e5ee2426f541
Stephan109-5 <t109990005@ntut.org.tw>
2024-06-14 23:46:16 +0800

    modify auth rules

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -8,7 +8,7 @@ export const storage = defineStorage({
       allow.entity('identity').to(['read', 'write', 'delete'])
     ],
     'picture-submissions/*': [
-      allow.authenticated.to(['read','write']),
+      allow.authenticated.to(['read','write', 'delete']),
       allow.guest.to(['read', 'write'])
     ],
   })
